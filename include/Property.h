aef822490a87b3e4be2c04e8861afe5012cbfb6e
Mason <mason@hicksm.dev>
2024-06-12 13:28:58 -0400

    Property now determines if Type is an appropriate match

--- include/Property.h
+++ include/Property.h
@@ -1,10 +1,18 @@
 #ifndef cppml_Property_h
 #define cppml_Property_h
 
+#include "Type.h"
+
 namespace cppml {
 
     /// @brief parent of all property indicators for a data attribute
-    class Property {};
+    class Property {
+
+    public:
+
+        virtual void validate(Type&) = 0;
+
+    };
 
 }
 
